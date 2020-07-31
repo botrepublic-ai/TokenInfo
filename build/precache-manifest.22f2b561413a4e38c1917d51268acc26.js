49e7d771de973df79c861409b9bb2238267d8f0c
botrepublic-ai <goodbot20170715@gmail.com>
2020-07-31 16:27:36 +0900

    init

---
+++ build/precache-manifest.22f2b561413a4e38c1917d51268acc26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e1146c7c9bd599da3984a26f9836ea1",
+    "url": "/TokenInfo/index.html"
+  },
+  {
+    "revision": "bd6545d84f7b12f93b50",
+    "url": "/TokenInfo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b2082408844a3d549244",
+    "url": "/TokenInfo/static/js/2.6025b754.chunk.js"
+  },
+  {
+    "revision": "bd32164b0b1f07ae79545cc1dff28ec5",
+    "url": "/TokenInfo/static/js/2.6025b754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd6545d84f7b12f93b50",
+    "url": "/TokenInfo/static/js/main.3f101152.chunk.js"
+  },
+  {
+    "revision": "dc2218fc7a8bfcd4d51a",
+    "url": "/TokenInfo/static/js/runtime-main.6f0fbd1b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TokenInfo/static/media/logo.5d5d9eef.svg"
+  }
+]);
